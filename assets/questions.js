434ff6862b13204c3fb51f151764814818864d0e
muulinha <lucasfl.araujo@hotmail.com>
2022-12-03 07:54:29 +0800

    Adding questions

---
+++ assets/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+      title: 'Commonly used data types DO NOT include:',
+     options: ['strings', 'booleans', 'alerts', 'numbers'],
+      answer: 'alerts',
+    },
+    {
+        title: 'The condition in an if/else statement is encolosed within ____.',
+       options: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+        answer: 'parentheses',
+    },
+    {
+        title: 'Arrays in JavaScript can be used to store _____.',
+       options: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+        answer: 'all of the above',
+    },
+    {
+        title: 'String values must be enclosed within ______ when being assigned to variables.',
+       options: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+        answer: 'quotes',
+    },
+    {
+        title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+       options: ['JavaScript', 'terminal/bash', 'for loops', 'console log'],
+        answer: 'console log',
+    },
+];
--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
 // adding timer
 var timerEl = document.getElementById("timer");
-
+var questionsElement = document.querySelector('#questions');
 var secondsLeft = 60;
-
+var currentQuestionIndex = 0;
+var optionsElement = document.querySelector('#options')
 function setTime() {
   // Sets interval in variable
   var timerInterval = setInterval(function() {
@@ -25,52 +26,52 @@ var startButton = document.getElementById("startQuiz");
 var page1 = document.getElementById("page1");
 
 
-startButton.addEventListener("click", function() {
-  page1.remove();
-  setTime();
-});
+// startButton.addEventListener("click", function() {
+//   page1.remove();
+//   setTime();
+// });
+function startGame() {
+  var startGameScreen = document.querySelector('#page1')
+  startGameScreen.setAttribute('class', 'hide')
 
+  questionsElement.removeAttribute('class')
 
+  setTime()
 
+  askQuestion()
+}
 
+function askQuestion() {
+  var currentQuestion = questions[currentQuestionIndex]
 
+  var questionTitle = document.querySelector('#question-name')
+  questionTitle.textContent = currentQuestion.title;
 
+  for (i = 0; i < currentQuestion.options.length; i++) {
+    var option = currentQuestion.options[i]
+    
+    var optionsBtn = document.createElement('button');
+    optionsBtn.setAttribute('value', option)
 
-// [
-//     {
-//       title: 'Commonly used data types DO NOT include:',
-//       choices: ['strings', 'booleans', 'alerts', 'numbers'],
-//       answer: 'alerts',
-//     },
-//     {
-//         title: 'The condition in an if/else statement is encolosed within ____.',
-//         choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-//         answer: 'parentheses',
-//     }
-//     {
-//         title: 'Arrays in JavaScript can be used to store _____.',
-//         choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
-//         answer: 'all of the above',
-//     }
-//     {
-//         title: 'String values must be enclosed within ______ when being assigned to variables.',
-//         choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
-//         answer: 'quotes',
-//     }
-//     {
-//         title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
-//         choices: ['JavaScript', 'terminal/bash', 'for loops', 'console log'],
-//         answer: 'console log',
-//     }
-//   ]
+    optionsBtn.textContent = i + 1 + option
 
+    optionsElement.appendChild(optionsBtn)
+  }
+}
 
+// function for when a user clicks on a option
+// hint: look for a func example that takes a user click event 
+// this func will add and subtract time and display righ t or wrong feedback 
+// it will increment question index
+// if there are no more questions, run endGame, else rerun askQuestion
 
+// console.log(questionsAnswers[1]);
 
+//funct to end game 
 
+// func to save high score 
 
-
-
+startButton.onclick = startGame;
 
 
 
